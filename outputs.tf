2060c766ee54f923359761f4e4d415c9c58ae44a
emmanuel <emmanuelakoje@gmail.com>
2025-08-21 01:09:43 +0100

    gitpush

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "site_url" {
-  description = "The live URL of the Netlify site"
-  value       = data.netlify_site.this.url
-}
-
 output "site_id" {
   description = "The Netlify site ID"
   value       = data.netlify_site.this.id
+}
+
+output "site_url" {
+  description = "The Netlify site URL"
+  value       = "https://${var.site_name}.netlify.app"
 }
