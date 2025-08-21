141290ef0d53c004efdf255f4406ed86b1acfbd2
emmanuel <emmanuelakoje@gmail.com>
2025-08-21 01:20:27 +0100

    gitpush

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
-output "site_id" {
-  description = "The Netlify site ID"
-  value       = var.site_name   # Or leave blank if you don't need the ID
-}
-
+# outputs.tf
 output "site_url" {
   description = "The Netlify site URL"
   value       = "https://${var.site_name}.netlify.app"
+}
+
+output "site_id" {
+  description = "The Netlify site ID"
+  value       = var.site_name
 }
